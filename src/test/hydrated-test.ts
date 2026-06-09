bd4eb30e0fe4877d9438df9c3743f9abe0cdfc1a
Michel Guerin <dev@guerin.at>
2026-06-09 14:40:00 +0200

    fix(test): change default namespace

--- src/test/hydrated-test.ts
+++ src/test/hydrated-test.ts
@@ -14,9 +14,8 @@ import type { Config } from '@/config/dialecte.config'
 
 type PlcModules = typeof PLC_EXTENSION_MODULES
 
-export const XMLNS_SCL_NAMESPACE = `xmlns="http://www.iec.ch/61850/2003/SCL"`
-export const XMLNS_SCL_6_100_NAMESPACE = `xmlns:eIEC61850-6-100="http://www.iec.ch/61850/2019/SCL/6-100"`
-export const ALL_XMLNS_NAMESPACES = `${XMLNS_SCL_NAMESPACE} ${XMLNS_SCL_6_100_NAMESPACE} ${XMLNS_DEV_NAMESPACE}`
+export const XMLNS_PLC_NAMESPACE = `xmlns=${PLC_DIALECTE_CONFIG.namespaces.default.uri}`
+export const ALL_XMLNS_NAMESPACES = `${XMLNS_PLC_NAMESPACE} ${XMLNS_DEV_NAMESPACE}`
 export { CUSTOM_RECORD_ID_ATTRIBUTE, CUSTOM_RECORD_ID_ATTRIBUTE_NAME }
 
 const PLC_EXTENSIONS = { base: PLC_EXTENSION_MODULES }
